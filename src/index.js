41d01464a4b4d92d8e913c8bdcaedc08101a08e3
Flashspeed <flashspeed@hotmail.co.uk>
2019-06-14 03:08:43 +0100

    Asset Management section complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import _ from 'lodash';
+import './style.css';
+import ImageLew from './lew.png';
+import DataFile from './data.xml';
 
 function component() {
+    // Create a div
     const element = document.createElement('div');
 
+    // Add "Hello webpack" text inside the div
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Add a class named "hello" to the div
+    element.classList.add("hello");
+
+    // Create an image element
+    const imageOfLew = new Image();
+    imageOfLew.src = ImageLew;
+
+    // Append image element to the existing div
+    element.appendChild(imageOfLew);
+
+    console.log(DataFile);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,43 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: "main.js",
+        // The name of the final js file to be embedded into the main html file
+        filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                // Test to see if this condition is met. Condition: if a file ends with '.css'
+                /* Webpack uses a regular expression to determine which files it should look for
+                   and serve to a specific loader
+               */
+                test: /\.css$/,
+
+                //Webpack loaders are read from last to first. Here 'css-loader' runs before 'style-loader'
+                use: [
+                    'style-loader', // Includes the parsed css into the DOM by injecting a <style> tag.
+                    'css-loader' // Parses css
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 };
